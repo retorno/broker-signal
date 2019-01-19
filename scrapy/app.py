91097984036e4eee18624114235a2772844198a5
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2019-01-19 00:11:40 -0200

    fix app broker

--- scrapy/app.py
+++ scrapy/app.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 import os, time, json
 from flask import Flask, request, Response
-from broker.scrapy_clear import ScrapyClear
-from broker.scrapy_clear import BrokerRoles
+from scrapy.scrapy_clear import ScrapyClear
+
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -16,45 +16,45 @@ clear = None
 debug = False
 
 
-@app.route('/broker/position', methods=['GET'])
+@app.route('/scrapy/position', methods=['GET'])
 def getPosition():
     position = clear.getPosition()
     return str(position)
 
 
-@app.route('/broker/last-price', methods=['GET'])
+@app.route('/scrapy/last-price', methods=['GET'])
 def getLastPrice():
     lastPrice = clear.getLastPrice()
     return str(lastPrice)
 
 
-@app.route('/broker/recipe', methods=['GET'])
+@app.route('/scrapy/recipe', methods=['GET'])
 def getRecipe():
     recipe = clear.getRecipe()
     return recipe
 
 
-@app.route('/broker/zerar-all', methods=['GET'])
+@app.route('/scrapy/zerar-all', methods=['GET'])
 def zeraAll():
     zerar = clear.zeraAll()
     return zerar
 
 
-@app.route('/broker/set-order', methods=['POST'])
+@app.route('/scrapy/set-order', methods=['POST'])
 def setOrder():
     order = clear.setOrderFast(stock=request.json)
     return order
 
 
-@app.route('/broker/set-stop', methods=['POST'])
+@app.route('/scrapy/set-stop', methods=['POST'])
 def setStop():
     order = clear.setStop(stock=request.json)
     return order
 
 
-@app.route('/broker/cancel-order', methods=['POST'])
+@app.route('/scrapy/cancel-order', methods=['GET'])
 def cancelOrder():
-    order = clear.cancelOrders(stock=request.json)
+    order = clear.cancelOrders()
     return order
 
 
@@ -70,5 +70,5 @@ def connectBroker():
 
 if __name__ == '__main__':
     connectBroker()
-    # sudo lsof -t -i tcp:5018 | xargs kill
-    app.run(debug=debug, host='0.0.0.0', port=5018)
+    # sudo lsof -t -i tcp:5010 | xargs kill
+    app.run(debug=debug, host='0.0.0.0', port=5010)
